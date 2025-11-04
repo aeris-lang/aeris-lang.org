f7132046b322f24994b5a98538626f8310288699
PhillY <philly706869@gmail.com>
2025-11-04 19:18:40 +0000

    2025-11-05

---
+++ src/lib/components/Intro.svelte
@@ -0,0 +1,97 @@
+<script lang="ts">
+  import { m } from "$lib/paraglide/messages";
+</script>
+
+<section class="container">
+  <div class="content">
+    <div class="desc">
+      <h1>
+        <span>{m.home_intro_h1_1()}</span>
+        <span>{m.home_intro_h1_2()}</span>
+      </h1>
+      <h2>{m.home_intro_h2()}</h2>
+    </div>
+    <nav>
+      <a href="/">{m.home_intro_get_started()}</a>
+      <a href="/">Learn AERIS</a>
+    </nav>
+  </div>
+</section>
+
+<style lang="scss">
+  .container {
+    container-type: inline-size;
+  }
+
+  .content {
+    font-size: max(1cqw, 0.6rem);
+    padding: 10em 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 4em;
+  }
+
+  .desc {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 2em;
+  }
+
+  h1 {
+    font-size: 4em;
+    line-height: 1.2;
+    font-weight: 700;
+    display: grid;
+    grid-auto-flow: row;
+    grid-auto-rows: 1em;
+    place-items: center;
+
+    span {
+      background: linear-gradient(to top, var(--color-subtext) 0%, var(--color-accent) 50%);
+      background-clip: text;
+      color: transparent;
+    }
+  }
+
+  h2 {
+    font-size: 2.5em;
+    line-height: 1;
+    font-weight: 600;
+    color: var(--color-subtext);
+  }
+
+  nav {
+    display: flex;
+    gap: 1em;
+
+    a {
+      font-size: 0.8em;
+      line-height: 1;
+      padding: 0.8em 1.4em;
+      display: block;
+      border-radius: 0.4em;
+      transition: 100ms ease-out;
+      transition-property: background-color;
+      user-select: none;
+    }
+
+    a:first-child {
+      color: var(--color-bg);
+      background-color: var(--color-accent);
+
+      &:hover {
+        background-color: var(--color-text);
+      }
+    }
+
+    a:not(:first-child) {
+      border: 1px solid var(--color-line);
+
+      &:hover {
+        background-color: rgb(from var(--color-line) r g b / 0.4);
+      }
+    }
+  }
+</style>
--- src/routes/[...locale]/+page.svelte
+++ src/routes/[...locale]/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
+  import Intro from "$lib/components/Intro.svelte";
   import Lettermark from "$lib/components/Lettermark.svelte";
-  import SampleCode from "$lib/components/SampleCode.svelte";
   import { m } from "$lib/paraglide/messages";
 </script>
 
@@ -13,23 +13,7 @@
   </nav>
 </header>
 <main>
-  <section class="intro">
-    <div>
-      <div class="desc">
-        <h1>
-          <span>{m.home_intro_h1_1()}</span>
-          <span>{m.home_intro_h1_2()}</span>
-        </h1>
-        <h2>{m.home_intro_h2()}</h2>
-      </div>
-      <nav>
-        <a href="/">{m.home_intro_get_started()}</a>
-      </nav>
-    </div>
-    <div>
-      <SampleCode />
-    </div>
-  </section>
+  <Intro />
   <section class="about">
     <h1>Why AERIS?</h1>
     <div>
@@ -87,75 +71,6 @@
 
       &:hover {
         color: var(--color-accent);
-      }
-    }
-  }
-
-  .intro {
-    padding: 10%;
-    display: flex;
-    gap: 1rem;
-
-    .desc {
-      display: flex;
-      flex-direction: column;
-      align-items: flex-start;
-      gap: 2rem;
-    }
-
-    h1 {
-      width: max-content;
-      display: grid;
-      grid-template-rows: repeat(2, 4rem);
-      justify-content: flex-start;
-      align-items: center;
-
-      span {
-        font-size: 4rem;
-        font-weight: 700;
-        background: linear-gradient(to top, var(--color-subtext) 0%, var(--color-accent) 50%);
-        background-clip: text;
-        color: transparent;
-      }
-    }
-
-    h2 {
-      font-size: 2.5rem;
-      line-height: 2.5rem;
-      font-weight: 600;
-      color: var(--color-subtext);
-    }
-
-    nav {
-      width: max-content;
-      display: flex;
-      gap: 1rem;
-
-      a {
-        font-size: 1rem;
-        line-height: 1rem;
-        padding: 0.8rem 1.5rem;
-        display: block;
-        border-radius: 8px;
-        transition: 100ms ease-out;
-        user-select: none;
-      }
-
-      a:first-child {
-        color: var(--color-bg);
-        background-color: var(--color-accent);
-
-        &:hover {
-          background-color: var(--color-text);
-        }
-      }
-
-      a:not(:first-child) {
-        border: 1px solid var(--color-line);
-
-        &:hover {
-          background-color: rgb(from var(--color-line) r g b / 0.4);
-        }
       }
     }
   }
