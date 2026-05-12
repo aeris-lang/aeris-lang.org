0b5463de5c4962e0676cc78f1aad00f7a6912aea
PhillY <philly706869@gmail.com>
2026-05-13 00:44:26 +0900

    2026-05-13

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,4 @@
-import { includeIgnoreFile } from "@eslint/compat";
+import { includeIgnoreFile } from "@eslint/config-helpers";
 import js from "@eslint/js";
 import prettier from "eslint-config-prettier";
 import svelte from "eslint-plugin-svelte";
