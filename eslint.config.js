ec47f8180881f04a68de368a6216f66929cf8a87
PhillY <philly706869@gmail.com>
2026-05-17 16:16:21 +0000

    2026-05-18

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
-import { defineConfig, includeIgnoreFile } from "@eslint/config-helpers";
 import js from "@eslint/js";
 import prettier from "eslint-config-prettier";
 import svelte from "eslint-plugin-svelte";
+import { defineConfig, includeIgnoreFile } from "eslint/config";
 import globals from "globals";
 import path from "node:path";
 import ts from "typescript-eslint";
@@ -18,9 +18,14 @@ export default defineConfig(
   svelte.configs.prettier,
   {
     languageOptions: {
-      globals: { ...globals.browser, ...globals.node },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
     },
-    rules: { "no-undef": "off" },
+    rules: {
+      "no-undef": "off",
+    },
   },
   {
     files: ["**/*.svelte", "**/*.svelte.ts"],
